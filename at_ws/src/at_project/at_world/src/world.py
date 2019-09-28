651283addd9289016386b1fddcc918cc90a35a4f
vauhochzett <5780401+vauhochzett@users.noreply.github.com>
2019-09-28 17:53:53 +0200

    world: Adds basic service '/agent1/next_move'

--- at_ws/src/at_project/at_world/src/world.py
+++ at_ws/src/at_project/at_world/src/world.py
@@ -2,13 +2,16 @@
 
 import rospy
 
+# pylint: disable-msg=no-name-in-module
 from at_msgs.msg import PositionMsg
+from at_msgs.srv import NextMoveSrv
 
 
 class World(object):
     def __init__(self):
         rospy.init_node("world")
         self.test_pos_pub = rospy.Publisher("/agent1/position", PositionMsg, queue_size=1)
+        self.nm_srv = rospy.Service("/agent1/next_move", NextMoveSrv, self.next_move)
 
     def simulate(self):
         rate = rospy.Rate(30)
@@ -21,6 +24,10 @@ class World(object):
             self.test_pos_pub.publish(pos)
             rospy.loginfo("Published")
             rate.sleep()
+
+    def next_move(self):
+        # 0 Noop, 1 Forwards, 2 Right, 3 Backwards, 4 Left
+        return { "msg_id": 0, "move": 1 }
 
 
 if __name__ == "__main__":
